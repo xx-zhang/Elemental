3d0132841be2127761f0f887b682baa083ffa2ac
root <root@DESKTOP-8283LL9.localdomain>
2021-07-29 13:50:04 +0800

    [修改为国人能看的目录结构]

---
+++ docs/containers/adip-scripts/local-bootstrap.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+fucntion common_tools(){
+
+  apt-get -y install wget curl make tcpreplay nmap masscan
+}
+
+function install_tcpreplay(){
+
+
+
+
+}
+
+function install_dpdk_pdump(){
+
+
+
+}
+
+
+function install_scan_utils(){
+
+
+}
+
